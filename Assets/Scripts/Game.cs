6fd4bb0a9d1b1a6995dc8ba3828c4958a38d13e7
GreatVV <marinegreatvv@yahoo.com>
2018-08-09 20:14:02 +0300

    Изменения в класс game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,16 +4,17 @@ public class Game : MonoBehaviour
 {
     public GlobalStateContext Context;
 
+    public void Awake()
+    {
+        UnitManager.Instance = new UnitManager();
+        EventDispatcher.Instance = new EventDispatcher();
+    }
 
     public void Start()
     {
         var unitFactory = new UnitFactory(Context, new MockFactory(Context));
         var unit = unitFactory.CreateUnit();
-        UnitManager.Instance = new UnitManager();
         UnitManager.Instance.AddUnit(unit);
-
-        EventDispatcher.Instance = new EventDispatcher();
-
     }
 
 }
